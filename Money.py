649896e3799d81b3b9e87ea1b962f2809a974d3d
SilentTremor <eugeneatschool@gmail.com>
2015-12-01 16:30:11 -0600

    Move to zip
    Moved the python files to a zip folder

--- Money.py
+++
@@ -1,8 +0,0 @@
-def Quarter(pen):
-	return pen/25
-def Dime(pen):
-	return pen/10
-def Nickel(pen):
-	return pen/5
-def Penny(pen):
-	return pen
--- Time.py
+++
@@ -1,6 +0,0 @@
-def hours(sec):
-	return sec/3600
-def minutes(sec):
-	return sec/60
-def seconds(sec):
-	return sec
